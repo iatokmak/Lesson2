8d675df93add184d6aaaeb29492b9143d3bddd0b
Ibragim Tokmakov <iatokmakov@gmail.com>
2024-11-06 21:53:41 +0300

    Initial Commit

---
+++ Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by Ibragim Tokmakov on 06.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
